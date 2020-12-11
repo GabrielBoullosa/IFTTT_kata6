312e9f06badfee1d20920b4b6e50480355ec9a76
GabrielBR16 <gabrieldiques@gmail.com>
2020-12-11 18:51:37 +0000

    The relational operator need an operator an this consist on concrete values.

---
+++ src/main/java/es/ulpgc/dis/Operator.java
@@ -0,0 +1,4 @@
+package es.ulpgc.dis;
+
+public enum Operator {EQUALS, NOT_EQUAL, GREATER_THAN,
+    LESS_THAN, GREATER_EQUAL_THAN, LESS_EQUAL_THAN}
--- src/main/java/es/ulpgc/dis/RelationalOperator.java
+++ src/main/java/es/ulpgc/dis/RelationalOperator.java
@@ -1,5 +1,9 @@
 package es.ulpgc.dis;
 
 public abstract class RelationalOperator {
+    private Operator operator;
 
+    public RelationalOperator(Operator operator) {
+        this.operator = operator;
+    }
 }
